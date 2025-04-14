4a81beef26f2157c753c520fb1ca78745c599b19
bigos <michal.rosikiewicz@gmail.com>
2025-04-15 00:39:54 +0200

    lint fixes, added lint threshold

--- wow-recorder/obs/obs_control.py
+++ wow-recorder/obs/obs_control.py
@@ -1,53 +1,51 @@
+"""OBS Controller using web socket"""
 import obsws_python as obs
-import psutil
-from obsws_python.error import OBSSDKError
 
 
 class OBSController:
+    """Object for controlling OBS Studio via web socket"""
 
     def __init__(self, host, port, password):
-        self.OBS_PROCESS_NAME = 'obs'
         self.connected = False
         self.obs_client = None
         self.host = host
         self.port = port
         self.password = password
 
-    def is_running(self):
-        for process in psutil.process_iter():
-            if self.OBS_PROCESS_NAME == process.name():
-                return True
-        return False
-
     def connect(self):
+        """Connects to OBS"""
         try:
             self.obs_client = obs.ReqClient(host=self.host, port=self.port, password=self.password)
             self.connected = True
             return True
-        except ConnectionRefusedError as e:
+        except ConnectionRefusedError:
             return False
 
 
     def disconnect(self):
+        """Disconnects from OSB"""
         if self.connected:
             self.obs_client.disconnect()
             self.connected = False
 
     def get_record_status(self):
+        """Indicates whether recording is happening"""
         if not self.connected:
-            raise Exception("OBS not connected...")
+            raise RuntimeError("OBS not connected...")
 
         return self.obs_client.get_record_status()
 
     def start_recording(self):
+        """Starts recording"""
         if not self.connected:
-            raise Exception("OBS not connected...")
+            raise RuntimeError("OBS not connected...")
 
         return self.obs_client.start_record()
 
     def end_recording(self):
+        """Ends recording in OBS"""
         if not self.connected:
-            raise Exception("OBS not connected...")
+            raise RuntimeError("OBS not connected...")
 
         result = self.obs_client.stop_record()
         return result.output_path
