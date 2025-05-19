feb625cf75aa252e66497a7232a44be35d288eb0
bigos <michal.rosikiewicz@gmail.com>
2025-05-19 12:51:19 +0200

    toString and recorder configuration tests added

--- tests/test_wow_recorder.py
+++ tests/test_wow_recorder.py
@@ -1,7 +1,7 @@
 import datetime
 from time import sleep
 
-from src.wow_recorder import Activity, ActivityType, make_file_name
+from src.wow_recorder import Activity, ActivityType, make_file_name, get_file_extension, RecorderConfiguration
 
 name = 'Name'
 key_level = 12
@@ -26,6 +26,11 @@ def test_add_event():
     assert len(act.events) == 1
     assert act.events[0]['time'] == '00:00:01'
     assert act.events[0]['event'] == event
+
+
+def test_activity_tostring():
+    act = Activity(activity_type=ActivityType.RAID, name=name, key_level=key_level, player_count=player_count)
+    assert str(act) == 'ActivityType.RAID, Name (0)'
 
 
 def test_make_file_name():
@@ -57,3 +62,23 @@ def test_make_file_name():
     assert 'kill' in file_name
     assert '.mkv' in file_name
 
+
+def test_get_file_extension():
+    file = 'filename.asd'
+    assert get_file_extension(file) == 'asd'
+
+    file = 'filename'
+    assert get_file_extension(file) == ''
+
+
+def test_recorder_configuration():
+    target_folder = 'tf'
+    boss_reset = 30
+    death_delay = 3
+    linger_time = 5
+    rc = RecorderConfiguration(recording_target_folder=target_folder, boss_reset=boss_reset, death_delay_seconds=death_delay, linger_time_seconds=linger_time)
+
+    assert rc.recording_target_folder == target_folder
+    assert rc.boss_reset == boss_reset
+    assert rc.death_delay_seconds == death_delay
+    assert rc.linger_time_seconds == linger_time
